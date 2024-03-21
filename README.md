f993429ee8504bb760fd2798d99de2ce50e1845b
seanpden <seanden522@gmail.com>
2024-03-20 22:34:07 -0500

    lots...

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-add checking for cargo crates
-fix status restarting after sudo request
-add loading notice for installs
+- [ ] add checking for cargo crates
+- [ ] fix status restarting after sudo request
+- [ ] add loading notice for installs
--- installer.sh
+++ installer.sh
@@ -85,202 +85,137 @@ set -e
 
 
 function init {
-  _task "apt-get Update and Upgrade"
-  _cmd "sudo apt-get update && sudo apt-get upgrade -y"
+    _task "apt-get Update and Upgrade"
+    _cmd "sudo apt-get update && sudo apt-get upgrade -y"
 }
 
 
-function install_git {
-  _task "Checking for Git"
-  if ! dpkg -s git >/dev/null 2>&1; then
-      _task "Installing Git"
-          _cmd "sudo apt-get install -y git-all"
-  fi
+function install_packages {
+    while IFS=, read -r name package; do
+        _task "Checking for $name"
 
+        if ! dpkg -s $package >/dev/null 2>&1; then
+            _task "Installing $name"
+            _cmd "sudo apt-get install -y $package"
+        fi
+    done < $1
 }
 
 
 function install_rust {
-  _task "Installing/Updating Rust"
-  _cmd "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
-}
-
-
-function install_python {
-  _task "Checking for Python3"
-  # Check if python3 and pip is installed
-  if ! dpkg -s python3 >/dev/null 2>&1; then
-      _task "Installing Python3"
-          _cmd "sudo apt-get install -y python3"
-  fi
-
-  _task "Checking for Pip"
-  if ! dpkg -s python3-pip >/dev/null 2>&1; then
-      _task "Installing Python3 Pip"
-          _cmd "sudo apt-get install -y python3-pip"
-  fi
+    _task "Installing/Updating Rust"
+    _cmd "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
 }
 
 
 function install_go {
-  _task "Checking for Go"
-  if [ ! -d /usr/local/go ]; then
-      _task "Installing Go"
-          _cmd "wget 'https://go.dev/dl/go1.22.0.linux-amd64.tar.gz'"
-          _cmd "sudo rm -rf /usr/local/go"
-          _cmd "sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz"
-          _cmd "sudo rm -rf go1.22.0.linux-amd64.tar.gz"
-          # echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-  fi
+    _task "Checking for Go"
+    if [ ! -d /usr/local/go ]; then
+        _task "Installing Go"
+        _cmd "wget 'https://go.dev/dl/go1.22.0.linux-amd64.tar.gz'"
+        _cmd "sudo rm -rf /usr/local/go"
+        _cmd "sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz"
+        _cmd "sudo rm -rf go1.22.0.linux-amd64.tar.gz"
+        # echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+    fi
 }
 
 
 function install_crates {
-  _task "Installing Starship"
-  _cmd "cargo install starship --locked"
-  _task "Installing  Bat"
-  _cmd "cargo install --locked bat"
-  _task "Installing Eza"
-  _cmd "cargo install eza"
-  _task "Installing  Fd-find"
-  _cmd "cargo install fd-find"
-  _task "Installing GitUI"
-  _cmd "cargo install gitui"
-  _task "Installing RipGrep"
-  _cmd "cargo install ripgrep"
-  _task "Installing SCCache"
-  _cmd "cargo install sccache --locked"
-  _task "Installing Zellij"
-  _cmd "cargo install --locked zellij"
-  _task "Installing Nu"
-  _cmd "cargo install nu"
-}
+    while IFS=, read -r name package; do
+        _task "Checking for $name"
 
+        if [ ! -f ~/.cargo/bin/$package ]; then
+            _task "Installing $name"
+            _cmd "cargo install $package --locked"
+        fi
+    done < $1
 
-function install_neofetch {
-  _task "Checking for Neofetch"
-  if ! dpkg -s neofetch >/dev/null 2>&1; then
-      _task "Installing Neofetch"
-          _cmd "sudo apt-get install -y neofetch"
-  fi
-
+    # _task "Installing Starship"
+    # _cmd "cargo install starship --locked"
+    # _task "Installing Bat"
+    # _cmd "cargo install --locked bat"
+    # _task "Installing Eza"
+    # _cmd "cargo install eza"
+    # _task "Installing GitUI"
+    # _cmd "cargo install gitui"
+    # _task "Installing Zellij"
+    # _cmd "cargo install --locked zellij"
+    # _task "Installing Nu"
+    # _cmd "cargo install nu"
 }
 
 
 function install_nvim {
-  _task "Checking for NeoVim"
-  if [ ! -f ~/nvim.appimage ]; then
-    _task "Installing NeoVim"
-        _cmd "curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
-        _cmd "sudo rm -rf /opt/nvim"
-        _cmd "sudo tar -C /opt -xzf nvim-linux64.tar.gz"
-        echo "export PATH=$PATH:/opt/nvim-linux64/bin" >> ~/.bashrc
-  fi
+    _task "Checking for NeoVim"
+    if [ ! -f ~/nvim.appimage ]; then
+        if [ ! -f /usr/bin/nvim ]; then
+            if [ ! -d opt/nvim-linux64x/bin/ ]; then
+                _task "Installing NeoVim"
+                _cmd "curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
+                _cmd "sudo rm -rf /opt/nvim"
+                _cmd "sudo tar -C /opt -xzf nvim-linux64.tar.gz"
+                echo "export PATH=$PATH:/opt/nvim-linux64/bin" >> ~/.bashrc
+            fi
+        fi
+    fi
 }
 
 
 function install_nvm {
-  _task "Checking for NVM"
-  if [ ! -d ~/.nvm/.git ]; then
-    _task "Installing NVM"
+    _task "Checking for NVM"
+    if [ ! -d ~/.nvm/.git ]; then
+        _task "Installing NVM"
         _cmd "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash"
-  fi
+    fi
 }
 
 
 function install_node {
-  _task "Checking for Node"
-  if ! which node >/dev/null; then
-    _task "Installing Node/NPM"
+    _task "Checking for Node"
+    if ! which node >/dev/null; then
+        _task "Installing Node/NPM"
         _cmd "nvm install node"
-  fi
-}
-
-
-function install_make {
-  _task "Checking for Make"
-  if ! dpkg -s make >/dev/null 2>&1; then
-      _task "Installing Make"
-          _cmd "sudo apt-get install -y make"
-  fi
-}
-
-
-function install_pkgconfig {
-  _task "Checking for Pkg-Config"
-  if ! dpkg -s pkg-config >/dev/null 2>&1; then
-      _task "Installing Pkg-Config"
-          _cmd "sudo apt-get install -y pkg-config"
-  fi
-}
-
-
-function install_libssl {
-  _task "Checking for LibSSL-Dev"
-  if ! dpkg -s libssl-dev >/dev/null 2>&1; then
-      _task "Installing LibSSL-Dev"
-          _cmd "sudo apt-get install -y libssl-dev"
-  fi
-}
-
-
-function install_cmake {
-  _task "Checking for CMake"
-  if ! dpkg -s cmake >/dev/null 2>&1; then
-      _task "Installing CMake"
-          _cmd "sudo apt-get install -y cmake"
-  fi
+    fi
 }
 
 
 function install_lvim {
-  _task "Checking for LVIM"
-  if ! which lvim >/dev/null; then
-    _task "Installing LVIM"
+    _task "Checking for LVIM"
+    if ! which lvim >/dev/null; then
+        _task "Installing LVIM"
         _cmd "LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)"
-  fi
+    fi
 
 }
 
 
 function install_dotfiles {
-  _task "Checking for dotfiles folder"
-  if [ ! -d ~/dotfiles ]; then
-    _task "Installing dotfiles"
+    _task "Checking for dotfiles folder"
+    if [ ! -d ~/dotfiles ]; then
+        # backup
+        # .bashrc, .config/lvim/config.lua, .config/lvim/lazy-lock.json, .config.neofetch/config.conf
+        # also move dashboard.lua to
+        # home/$USERNAME/.local/share/lunarvim/lvim/lua/lvim/core/alpha/dashboard.lua
+        _task "Installing dotfiles"
         _cmd "git clone https://github.com/seanpden/dotfiles.git ~/dotfiles/"
         _cmd "stow ~/dotfiles/."
-  fi
-}
-
-
-function install_gnu_stow {
-  _task "Checking for GNU Stow"
-  if ! dpkg -s stow >/dev/null 2>&1; then
-      _task "Installing GNU Stow"
-          _cmd "sudo apt-get install -y stow"
-  fi
+    fi
 }
 
 
 main () {
-  init
-  install_make
-  install_cmake
-  install_pkgconfig
-  install_libssl
-  install_git
-  install_rust
-  install_python
-  install_go
-  install_crates
-  install_neofetch
-  install_nvim
-  install_nvm
-  install_node
-  install_lvim
-  install_gnu_stow
-  install_dotfiles
-  _task_done
+    init
+    install_packages "apt-packages.txt"
+    install_rust
+    install_go
+    install_crates "crates.txt"
+    install_nvim
+    install_nvm
+    install_node
+    install_lvim
+    install_dotfiles
+    _task_done
 }
 
 
